43e50b526832f6f40e0b85e55227ec82eb7a5782
franciumnetwork-dev <franciumnetwork@gmail.com>
2024-05-17 17:28:05 -0400

    Update franciumuninstaller.bat

--- franciumuninstaller.bat
+++ franciumuninstaller.bat
@@ -12,14 +12,15 @@ echo.
 echo Are you sure you want to uninstall? (Y/N)
 set /p choice=
 if /I "%choice%" EQU "Y" (
+    echo.
     echo Thank you for using FranciumBP!!!
     pause
-    del /F /Q "franciumBP.bat"
-    del /F /Q "franciuminstaller.bat"
-    del /F /Q "franciumuninstaller.bat"
+    del /F /Q "franciumBP.bat" >nul 2>&1
+    del /F /Q "franciuminstaller.bat" >nul 2>&1
+    del /F /Q "franciumuninstaller.bat" >nul 2>&1
     cd /d "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
-    del /F /Q "startup.bat"
-    del /F /Q "startup.bat.new"
+    del /F /Q "startup.bat" >nul 2>&1
+    del /F /Q "startup.bat.new" >nul 2>&1
     exit
 ) else (
 exit
